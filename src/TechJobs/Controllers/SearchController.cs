77416ac1f4946efe57369ac74807722f74988033
Jonah Martinez <jonah.martinez01@outlook.com>
2018-11-29 00:56:07 -0500

    Fixed Error with Search Fuctionality

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
 
@@ -6,6 +7,7 @@ namespace TechJobs.Controllers
 {
     public class SearchController : Controller
     {
+        //static public List<Dictionary<string, string>> searchResults;
         public IActionResult Index()
         {
             ViewBag.columns = ListController.columnChoices;
@@ -15,6 +17,28 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+
+        //[HttpPost]
+        [Route("/Search/Results")]
+        public IActionResult Search(string searchType, string searchTerm)
+        {
+            List<Dictionary<String, String>> jobs;
+            if (searchType.Equals("all"))
+            {
+                jobs = JobData.FindByValue(searchTerm);
+                ViewBag.jobs = jobs;
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.jobs = jobs;
+            }
+
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Jobs with " + ListController.columnChoices[searchType] + ": " + searchTerm;
+            
+            return View("Index");
+        }
 
     }
 }
